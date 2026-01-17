2921ca0d29a0e21a77302fffc38cdc4919b988cf
Gurkirat900 <gs1683676@gmail.com>
2026-01-17 18:57:14 +0530

    created database pool instaed of standard createConnection for better integration with rds on aws

---
+++ src/app.js
@@ -0,0 +1,48 @@
+import express from "express"
+import cors from "cors"
+import 'dotenv/config'
+import cookieParser from "cookie-parser"
+import { errorHandler } from "./middlewares/errorHandler.js"
+import  userRoutes from './routers/userRouter.js';
+import societyRoutes from "./routers/societyRouter.js";
+import eventDraftRoutes from "./routers/eventDraftRouter.js"
+import eventRegistrationRoutes from "./routers/eventRegisterRouter.js"
+import eventRoutes from "./routers/eventsRouter.js"
+import eventFeedbackroutes from "./routers/eventFeedbackRouter.js"
+
+const app= express()
+
+const allowedOrigins = [
+  process.env.CORS_ORIGIN,     // prod frontend
+  "http://localhost:3000",     // teammate’s React dev
+  "http://localhost:5173"      // Vite dev server 
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) return callback(null, true);
+    return callback(new Error("Not allowed by CORS"));
+  },
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  exposedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }));
+
+
+// routes
+app.use("/api/v1/users",userRoutes);
+app.use("/api/v1/society",societyRoutes);
+app.use("/api/v1/event-draft",eventDraftRoutes);
+app.use("/api/v1/event",eventRoutes)
+app.use("/api/v1/event-register",eventRegistrationRoutes);
+app.use("/api/v1/event-feedback",eventFeedbackroutes);
+
+app.use(errorHandler);
+
+export default app;
--- src/config/db.js
+++ src/config/db.js
@@ -5,13 +5,18 @@ let db;   // connection instance=> queries will be called on this
 
 const dbconnect= async ()=> {                     // connection fn called in index.js
     try {
-        db= await mysql.createConnection({
+        if(!db){
+            db= mysql.createPool({
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASS,
-            database: process.env.DB_NAME
+            database: process.env.DB_NAME,
+            waitForConnections: true,
+            connectionLimit: 3,         // VERY IMPORTANT (Lambda safe)
+            queueLimit: 0
         })
-        console.log("Connected to MySQL: Success!");
+        }
+        console.log("Connected to MySQL: Success!!");
         return db;
     } catch (error) {
         console.log("ERROR! MySQL connection failed",error)
@@ -20,7 +25,7 @@ const dbconnect= async ()=> {                     // connection fn called in ind
 
 const getDB= ()=>{
     if(!db){
-        throw new ApiError(500,"Databse instance db not created")
+        throw new ApiError(500,"Databse pool db not created")
     }
     return db;
 }
