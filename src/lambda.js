53f2bfdb4b72661521af1a8ed0a31f542bec082f
Gurkirat900 <gs1683676@gmail.com>
2026-01-18 01:28:46 +0530

    added lambda.js for lambda fn in aws

---
+++ src/lambda.js
@@ -0,0 +1,20 @@
+import serverless from "serverless-http";
+import app from "./app.js";
+import { dbconnect } from "./config/db.js";
+
+let isDbConnected = false;
+
+const connectDBOnce = async () => {
+  if (!isDbConnected) {
+    await dbconnect();
+    isDbConnected = true;
+    console.log("DB connected (Lambda cold start)");
+  }
+};
+
+export const handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop= false;   // to prevent lambda waiting for open db sockets to close
+  await connectDBOnce();
+  return serverless(app)(event, context);     // lambda->handler->serverless->express
+};
+
