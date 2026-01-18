3a3b6ecc44339f4c9b6448b803f9090bd70db1c0
Gurkirat900 <gs1683676@gmail.com>
2026-01-18 18:00:53 +0530

    testing if any route is breaking lambda

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import userRoutes from "./routers/userRouter.js";
 
 const app = express();
 
@@ -33,8 +34,12 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/api/v1/users", userRoutes);
+
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
+
+
 
 export default app;
