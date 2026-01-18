0b2b38fa428126d0bb5a08a1e75ea02f78f6af76
Gurkirat900 <gs1683676@gmail.com>
2026-01-18 17:47:47 +0530

    testing if cors was breaking lambda(temporary)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
-// src/app.js
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
