00321e71b2acf3c8855b162fbda968cb2746ed4e
Gurkirat900 <gs1683676@gmail.com>
2026-01-18 18:19:39 +0530

    testing if any middleware or db is breaking lambda

--- src/lambda.js
+++ src/lambda.js
@@ -1,4 +1,19 @@
+// src/lambda.js
 import serverless from "serverless-http";
 import app from "./app.js";
+import { dbconnect } from "./config/db.js";
 
-export const handler = serverless(app);
+let isDbConnected = false;
+
+export const handler = serverless(app, {
+  async request(req, event, context) {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    if (!isDbConnected) {
+      await dbconnect();
+      isDbConnected = true;
+      console.log("DB connected (Lambda cold start)");
+    }
+  }
+});
+
