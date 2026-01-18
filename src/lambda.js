d2dc2ef506fcccf9ad5c9c61e02fbe808ae2f246
Gurkirat900 <gs1683676@gmail.com>
2026-01-18 17:34:40 +0530

    Testing for bugs

--- src/lambda.js
+++ src/lambda.js
@@ -1,20 +1,9 @@
-import serverless from "serverless-http";
-import app from "./app.js";
-import { dbconnect } from "./config/db.js";
-
-let isDbConnected = false;
-
-// Connect DB ONCE during cold start
-const handler = serverless(app, {
-  async request(request, event, context) {
-    context.callbackWaitsForEmptyEventLoop = false;
-
-    if (!isDbConnected) {
-      await dbconnect();
-      isDbConnected = true;
-      console.log("DB connected (Lambda cold start)");
-    }
-  }
-});
-
-export { handler };
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ ok: true })
+  };
+};
