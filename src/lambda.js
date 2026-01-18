84381ea458375b97351bde5bcef5449267e0ab0b
Gurkirat900 <gs1683676@gmail.com>
2026-01-18 17:13:42 +0530

    removed serverless from async wrapper to test if this is causing bug

--- src/lambda.js
+++ src/lambda.js
@@ -4,17 +4,17 @@ import { dbconnect } from "./config/db.js";
 
 let isDbConnected = false;
 
-const connectDBOnce = async () => {
-  if (!isDbConnected) {
-    await dbconnect();
-    isDbConnected = true;
-    console.log("DB connected (Lambda cold start)");
+// Connect DB ONCE during cold start
+const handler = serverless(app, {
+  async request(request, event, context) {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    if (!isDbConnected) {
+      await dbconnect();
+      isDbConnected = true;
+      console.log("DB connected (Lambda cold start)");
+    }
   }
-};
+});
 
-export const handler = async (event, context) => {
-  context.callbackWaitsForEmptyEventLoop= false;   // to prevent lambda waiting for open db sockets to close
-  await connectDBOnce();
-  return await serverless(app)(event, context);     // lambda->handler->serverless->express
-};
-
+export { handler };
